1093d8d45fc1eefef9657fa9fcc8ebfbb0d3c605
drelyea3 <relyea_d@msn.com>
2018-12-16 23:15:47 -0800

    SettingsControl

---
+++ MemoryClock/Controls/RadioToggle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace MemoryClock.Controls
+{
+    public class RadioToggle : RadioButton
+    {
+        public RadioToggle()
+        {
+            this.DefaultStyleKey = typeof(ToggleButton);
+        }
+    }
+}
