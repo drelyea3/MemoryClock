28698de2b7222c3c116be55f7ad46a188cd241ca
drelyea3 <relyea_d@msn.com>
2018-12-16 22:14:09 -0800

    Remove data attributes from settings

--- MemoryClock/Settings.cs
+++ MemoryClock/Settings.cs
@@ -1,27 +1,19 @@
 ﻿using Common;
 using System;
-using System.Runtime.Serialization;
 
 namespace MemoryClock
 {
-    [DataContract]
     public class Settings
     {
-        [DataMember]
         public int TickInterval { get; set; } = 100;
 
         // As the opacity values go from 0 to 1, the underlying screen gets darker.
         // 0 = brightest, 1 = darkest (can't see it at all)
-        [DataMember]
         public double OverlayOpacityBright { get; set; } = 0.0;
-        [DataMember]
         public double OverlayOpacityDim { get; set; } = 0.6;
 
-        [DataMember]
         public QuarterHourlySchedule<bool> IsDim { get; set; } = new QuarterHourlySchedule<bool>();
-        [DataMember]
         public QuarterHourlySchedule<bool> IsNoPhone { get; set; } = new QuarterHourlySchedule<bool>();
-        [DataMember]
         public QuarterHourlySchedule<string> Message { get; set; } = new QuarterHourlySchedule<string>();
 
         public Settings()
